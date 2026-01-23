a27326c0543e2106645dc73edf98b727e6198793
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-23 10:00:34 +0530

    update

--- digitalSum.cpp
+++ digitalSum.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 long long digitalSum(long long int num)
-{
+{   
     long long int digiSum = 0;
     while (num > 0) {
         digiSum += num % 10;
@@ -12,12 +12,22 @@ long long digitalSum(long long int num)
     if(digiSum>0 && digiSum<=9){
         return digiSum;
     }else{
+        
         long long int temp = 0;
-        while (digiSum >1){
+        while (digiSum >0 ){
             temp = temp + digiSum%10;
-            digiSum /= 10;
+            digiSum =digiSum/10;
         }
-        return temp;
+        
+        if(temp>0 and temp<=9){
+            return temp;
+        }else{
+            long long int temp1 = 0;
+            while (temp >0 ){
+                temp1 = temp1 + temp%10;
+                temp =temp/10;
+            }
+        }
     }
 }
 
@@ -26,6 +36,10 @@ int main()
     long long int num = 0;
     cout<<"Enter the number is : ";
     cin>>num;
-    cout << "Your Digital Sum is : " << digitalSum(num)<<endl;
+    // long long int temp = digiSum(num);
+    if(digitalSum(num)== 0){
+        cout<<"Your Digital Sum is : 0 or 9 "<<endl;
+    }else{
+    cout << "Your Digital Sum is : " <<digitalSum(num)<<endl;}
     return 0;
 }
