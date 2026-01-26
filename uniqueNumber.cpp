17e25c9e6f658d6afd9f41bfa3c50c2fe18ee984
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-26 08:06:56 +0530

    finalUsesOfLeetCodeSyntax

--- uniqueNumber.cpp
+++ uniqueNumber.cpp
@@ -4,22 +4,19 @@ using namespace std;
 
 class Solution{
     public: 
-        //properties declaration 
-        vector <int> nums = {1,2,4,5,3,2,3,1,4};
-
         //member function 
         int singleNumber(vector<int>&nums){
             int ans = 0;
             for(int val: nums){
                 ans = ans^val;
-
             }
             return ans;
         }
-}
+};
 
 int main(){
+    vector <int> nums = {1,2,4,3,2,3,1};
     Solution T;
-    cout<<"Your Unique number is : "<<T.singleNumber()<<endl;
+    cout<<"Your Unique number is : "<<T.singleNumber(nums)<<endl;
     return 0;
 }
