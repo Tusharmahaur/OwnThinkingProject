f160003dbcf76eb990aeb2d059b310598a0bb027
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-01-26 08:45:49 +0530

    this

--- linearSearch.cpp
+++ linearSearch.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int linearSearch(int arr[]){
-    int target,size = 11;
-    cout<<"Enter the target : ";
-    cin>>target;
-
-    for(int i = 0; i<size; i++){
-        if(arr[i] == target){
-            return i+1;
+class Solution{
+    public:
+        int linearSearch(int arr[]){
+        int target,size = 11;
+        cout<<"Enter the target : ";
+        cin>>target;
+    
+        for(int i = 0; i<size; i++){
+            if(arr[i] == target){
+                return i+1;
+            }
         }
+        return -1;
     }
-    return -1;
-}
+};
 int main(){
+    Solution T;
     int arr[] = {2,3,4,-90,34,1,32,43,53,2,51};
     
-    cout<<"your target index is : "<<linearSearch(arr)<<endl;
+    cout<<"your target index is : "<<T.linearSearch(arr)<<endl;
 
     return 0;
 }
--- uniqueNumber.cpp
+++ uniqueNumber.cpp
@@ -15,7 +15,7 @@ class Solution{
 };
 
 int main(){
-    vector <int> nums = {1,2,4,3,2,3,1};
+    vector <int> nums = {1,2,4,3,2,3,1}; 
     Solution T;
     cout<<"Your Unique number is : "<<T.singleNumber(nums)<<endl;
     return 0;
