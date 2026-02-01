dfb929e41d86b2fec5a17f548631a5ed20a2c265
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-01 09:56:30 +0530

    this

--- dsa/binaryExponantion.cpp
+++ dsa/binaryExponantion.cpp
@@ -1,24 +1,36 @@
+//Here is timecomplexty is O(logn)
 #include <iostream>
 using namespace std;
 
 class Solution{
     public: 
         double myPow(){
-            long x;
+            double x;
             int n;
             cout<<"Enter the number and power respectivily : ";
             cin>>x>>n;
 
+            //corner case 
+            if(n==0) return 1.0;
+            if(x == 0) return 0.0;
+            if(x == 1) return 1.0;
+            if(x == -1 and n%2 == 0) return 1.0;
+            if(x == -1 and n%2 != 0) return -1.0;
+
+            
             long binForm = n;
             double ans = 1;
-
-
-            while(binForm>0){
-                    if(binForm%2 == 1){
-                ans = ans*x;
+            if(n<0){
+                x = 1/x;
+                binForm = -binForm;
             }
-            x *= x;
-            binForm/=2;
+
+            while(binForm >0){
+                if(binForm %2 == 1){
+                    ans *= x;
+                }
+                x *= x;
+                binForm /=2;
             }
             return ans;
         }
