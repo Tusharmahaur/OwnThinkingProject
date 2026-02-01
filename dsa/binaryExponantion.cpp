df1318ee6257794fb29eca913e1a35ff99ee6bdf
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-01 12:58:52 +0530

    upload

--- dsa/binaryExponantion.cpp
+++ dsa/binaryExponantion.cpp
@@ -1,44 +1,58 @@
-//Here is timecomplexty is O(logn)
-#include <iostream>
-using namespace std;
+// //Here is timecomplexty is O(logn)
+// #include <iostream>
+// using namespace std;
 
-class Solution{
-    public: 
-        double myPow(){
-            double x;
-            int n;
-            cout<<"Enter the number and power respectivily : ";
-            cin>>x>>n;
+// class Solution{
+//     public: 
+//         double myPow(){
+//             double x;
+//             int n;
+//             cout<<"Enter the number and power respectivily : ";
+//             cin>>x>>n;
 
-            //corner case 
-            if(n==0) return 1.0;
-            if(x == 0) return 0.0;
-            if(x == 1) return 1.0;
-            if(x == -1 and n%2 == 0) return 1.0;
-            if(x == -1 and n%2 != 0) return -1.0;
+//             //corner case 
+//             if(n==0) return 1.0;
+//             if(x == 0) return 0.0;
+//             if(x == 1) return 1.0;
+//             if(x == -1 and n%2 == 0) return 1.0;
+//             if(x == -1 and n%2 != 0) return -1.0;
 
             
-            long binForm = n;
-            double ans = 1;
-            if(n<0){
-                x = 1/x;
-                binForm = -binForm;
-            }
+//             long binForm = n;
+//             double ans = 1;
+//             if(n<0){
+//                 x = 1/x;
+//                 binForm = -binForm;
+//             }
 
-            while(binForm >0){
-                if(binForm %2 == 1){
-                    ans *= x;
-                }
-                x *= x;
-                binForm /=2;
-            }
-            return ans;
-        }
-};
+//             while(binForm >0){
+//                 if(binForm %2 == 1){
+//                     ans *= x;
+//                 }
+//                 x *= x;
+//                 binForm /=2;
+//             }
+//             return ans;
+//         }
+// };
 
+
+// int main(){
+//     Solution T;
+//     cout<<T.myPow()<<endl;
+//     return 0;
+// }
+
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main(){
-    Solution T;
-    cout<<T.myPow()<<endl;
+    short int ans = pow(4,2);
+    unsigned short int an = 0;
+    cout<<ans<<endl;
+    cout<<an<<endl;
+    cout<<sizeof(ans)<<endl;
+    cout<<sizeof(an)<<endl;
     return 0;
 }
---
+++ dsa/stockBuyAndSell.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public: 
+        int maxProfit(vector<int> &prices){
+            int maxProfit = 0, bestBuy = prices[0];
+
+            for(int i = 0; i<prices.size(); i++){
+                if(prices[i]>bestBuy){
+                    maxProfit = max(maxProfit,prices[i]-bestBuy);
+                }
+                bestBuy = min(bestBuy,prices[i]);
+            }
+            return maxProfit;
+
+        }
+
+
+};
+
+int main(){
+    vector<int> prices = {7,1,5,3,6,4};
+    cout<<"your maximum profit = "<<maxProfit(prices)<<endl;
+    return 0;
+}
