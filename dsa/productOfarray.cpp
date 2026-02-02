e95324798b4e99dd718228856f28a73015c6d766
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-02 20:09:42 +0530

    this

--- dsa/productOfarray.cpp
+++ dsa/productOfarray.cpp
@@ -1,4 +1,4 @@
-//time complexity O(n) linear time complexity 
+// //time complexity O(n) linear time complexity 
 // #include <iostream>
 // #include <vector>
 // using namespace std;
@@ -32,3 +32,42 @@
 //     }
 //     return 0;
 // }
+
+
+//Without using the division and time complexity O(n) and space complexity O(1)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution{
+    public: 
+        vector<int> proArray(vector<int>&nums){
+            int n = nums.size();
+            vector<int> ans(n,1);
+                       
+            //prefix
+            for(int i=1; i<n; i++){
+                ans[i] = ans[i-1]*nums[i-1];
+                
+            }
+            
+            int suffix = 1;
+            //suffix
+            for(int i = n-2; i>=0; i--){
+                suffix = suffix*nums[i+1];
+                ans[i] *= suffix;
+            }
+            return ans;
+        }
+};
+
+int main(){
+    Solution T;
+    vector<int> nums = {1,2,3,4};
+    vector<int> productArray = T.proArray(nums);
+
+    for(int val: productArray){
+        cout<<val<<" ";
+    }
+    return 0;
+}
