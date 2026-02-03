8a997c719e149cc717ce94fb8512672b1d4ac672
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-03 18:00:01 +0530

    logN

--- dsa/singleElement.cpp
+++ dsa/singleElement.cpp
@@ -40,7 +40,7 @@ class Solution{
 
                 if(mid == 0 && nums[0] != nums[1]) return nums[mid];
                 if(mid == n-1 && nums[n-1] != nums[n-2]) return nums[mid];
-
+ 
                 if(nums[mid-1] != nums[mid] and nums[mid] != nums[mid+1])return nums[mid];
 
                 if(mid%2 == 0){//even case me
@@ -50,7 +50,7 @@ class Solution{
                     }else{
                         st = mid+1;
                     }
-                }else{
+                }else{//odd case
                     if(nums[mid-1]== nums[mid]){
                         st = mid+1; 
                     }else{
