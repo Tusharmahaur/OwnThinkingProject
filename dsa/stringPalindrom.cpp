b40cbec6509afa6cea847a0506439fcfff0ad3f2
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-10 08:08:29 +0530

    palindromString

---
+++ dsa/stringPalindrom.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        bool isAlpha(char ch){
+            if((ch>='0' and ch<= '9')||(tolower(ch)>='a' && tolower(ch)<='z')){
+                return true;
+            }
+        }
+        bool isPalindrom(string s){
+            int st = 0, end = s.length()-1;
+            while(st<=end){
+                if(!isAlpha(s[st])){
+                    st++; continue;
+                }
+                if(!isAlpha(s[end])){
+                    end--; continue;
+                }
+
+                if(tolower(s[st]) != tolower(s[end])){
+                    return false;   
+                }
+
+                st++,end--;
+
+            }
+            return true;
+        }
+};
+
+int main(){
+    Solution T;
+    string str;
+
+    cout<<"Enter the string : ";
+    cin>>str;
+
+    int v = T.isPalindrom(str);
+    if(v == 1) cout<<"True"<<endl;
+    else cout<<"False"<<endl;
+
+    return 0;
+}
