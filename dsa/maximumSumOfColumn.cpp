5ab623f60532de9753c216009480e13a35440e1e
Tusharmahaur <Tusharmahaur73@gmail.com>
2026-02-12 20:23:39 +0530

    column matrix for maximum sum

---
+++ dsa/maximumSumOfColumn.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution{
+    public:
+        int maximumSumOf_Column(int arr[3][3]){
+            int maxSum = INT_MIN;
+            
+            for(int i = 0; i<3 ; i++){
+                int sum = 0;
+                for(int j =0 ; j<3 ; j++){
+                    sum += arr[j][i];
+                }
+                maxSum = max(maxSum,sum);
+            }
+            return maxSum;
+
+        }    
+
+};
+
+int main(){
+    Solution T;
+    int arr[3][3] = {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+
+    cout<<T.maximumSumOf_Column(arr)<<endl;
+    
+    return 0;
+}
